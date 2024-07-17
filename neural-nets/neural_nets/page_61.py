0a661d12627be879cd36d6c4f73e610aa4b83654
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-07-17 13:00:15 -0500

    Add files via upload

---
+++ neural-nets/neural_nets/page_61.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 12 11:49:39 2024
+
+@author: mark.patmore
+"""
+
+import numpy as np
+
+
+inputs = [[1.0, 2.0, 3.0, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
+
+weights = [[.2, .8, -0.5, 1],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+
+
+weights2 = [[.1, -0.14, 0.5],
+           [-0.5, 0.12, -0.33],
+           [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
+
+
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer2_outputs)
